35bd4d39720c8f34e04e5c6aa5ad56c06c97fbaf
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-12-14 17:08:44 +0600

    module complete

--- index.js
+++ index.js
@@ -89,6 +89,15 @@ async function run() {
   })
 
 
+  //app.get('/job-applications/:id')  ==> get a specific job application by id
+  app.get('/job-applications/jobs/:job_id', async(req, res) =>{
+    const jobId = req.params.job_id;
+    const query = { job_id: jobId };
+    const result = await jobApplicationCollection.find(query).toArray();
+    res.send(result)
+  })
+
+
   app.post('/job-applications', async(req, res) =>{
     const application = req.body;
     const result = await jobApplicationCollection.insertOne(application);
@@ -120,6 +129,20 @@ async function run() {
     const updateResult = await jobsCollection.updateOne(filter, updatedDoc);
     console.log(updateResult);
 
+    res.send(result);
+  })
+
+
+  app.patch('/job-applications/:id', async(req, res) =>{
+    const id = req.params.id;
+    const data = req.body;
+    const filter = { _id: new ObjectId(id)};
+    const updatedDoc = {
+      $set: {
+        status: data.status
+      }
+    }
+    const result = await jobApplicationCollection.updateOne(filter, updatedDoc);
     res.send(result);
   })
 
